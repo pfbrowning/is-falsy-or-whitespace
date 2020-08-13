a1b31064621fd3a3cdff44ec412ca4dce6600777
Patrick Browning <patrick@browninglogic.com>
2020-08-12 20:06:21 -0400

    Initial commit

---
+++ dist/index.d.ts
@@ -0,0 +1,1 @@
+export declare const isFalsyOrWhitespace: (value: string) => boolean;
---
+++ dist/index.js
@@ -0,0 +1,1 @@
+export var isFalsyOrWhitespace = function (value) { return !value || value.trim().length === 0; };
